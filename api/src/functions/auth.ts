1ad4c35a3a1c67e4c20140a16b2c16867d43bda9
Tobias Beese <tobias.beese@gmx.de>
2022-03-24 16:12:36 +0100

    Disable signup

--- api/src/functions/auth.ts
+++ api/src/functions/auth.ts
@@ -102,6 +102,8 @@ export const handler = async (event, context) => {
     // If this returns anything else, it will be returned by the
     // `signUp()` function in the form of: `{ message: 'String here' }`.
     handler: ({ username, hashedPassword, salt, userAttributes }) => {
+      return Promise.resolve(false)
+
       return db.user.create({
         data: {
           email: username,
