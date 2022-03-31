2acd60842515089426034e6b106f14e1d25fe48e
Tobias Beese <tobias.beese@gmx.de>
2022-03-31 22:51:18 +0200

    Imrpove feeding components and layout

--- web/src/layouts/FeedingLayout/FeedingLayout.tsx
+++ web/src/layouts/FeedingLayout/FeedingLayout.tsx
@@ -1,6 +1,8 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Toaster } from '@redwoodjs/web/dist/toast'
 
+import { Breast } from 'src/components/Icons'
+
 type FeedingLayoutProps = {
   children?: React.ReactNode
 }
@@ -8,14 +10,14 @@ type FeedingLayoutProps = {
 const FeedingLayout = ({ children }: FeedingLayoutProps) => {
   return (
     <>
-      <Toaster toastOptions={{ className: 'rw-toast', duration: 6_000 }} />
+      <Toaster toastOptions={{ className: 'rw-toast', duration: 3_000 }} />
       <div className="mb-4 flex items-baseline justify-between">
-        <h1 className="text-center text-xl">Stillen</h1>
+        <h1 className="flex-grow text-center text-2xl">Stillen</h1>
         <Link
-          className="rounded-full bg-emerald-600 py-2 px-4 font-bold text-white"
+          className="rounded-full bg-emerald-600 p-2"
           to={routes.startFeeding()}
         >
-          +
+          <Breast inverted />
         </Link>
       </div>
       {children}
