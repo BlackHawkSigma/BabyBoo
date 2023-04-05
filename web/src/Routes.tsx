8cfe99fa68cf7338238ccc2353acecbda4b22d3e
Tobias <tobias.beese@gmx.de>
2023-04-05 19:05:33 +0000

    Change prerendering

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -19,19 +19,18 @@ import FeedingLayout from './layouts/FeedingLayout/FeedingLayout'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
-      <Route path="/login" page={LoginPage} name="login" />
       <Route path="/login" page={LoginPage} name="login" prerender />
       {/* <Route path="/signup" page={SignupPage} name="signup" /> */}
-      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
-      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" prerender />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" prerender />
 
-      <Private unauthenticated="login" wrap={MainLayout} whileLoadingAuth={() => <WhileLoadingAuth />} prerender>
+      <Private unauthenticated="login" wrap={MainLayout} whileLoadingAuth={() => <WhileLoadingAuth />}>
         <Route path="/diaperchange" page={DiaperChangePage} name="diaperChange" />
 
         <Set wrap={WeightsLayout}>
-          <Route path="/weights/new" page={WeightNewWeightPage} name="newWeight" />
+          <Route path="/weights/new" page={WeightNewWeightPage} name="newWeight" prerender />
           <Route path="/weights/{id:Int}/edit" page={WeightEditWeightPage} name="editWeight" />
-          <Route path="/weights" page={WeightWeightsPage} name="weights" />
+          <Route path="/weights" page={WeightWeightsPage} name="weights" prerender />
         </Set>
 
         <Set wrap={FeedingLayout}>
@@ -41,7 +40,7 @@ const Routes = () => {
         </Set>
 
         <Route path="/user" page={UserPage} name="user" />
-        <Route path="/" page={HomePage} name="home" />
+        <Route path="/" page={HomePage} name="home" prerender />
       </Private>
 
       <Route notfound page={NotFoundPage} />
