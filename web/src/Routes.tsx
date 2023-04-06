ede4f30bc7ec8a6527f0b046cc769f77ed811243
Tobias <tobias.beese@gmx.de>
2023-04-06 09:51:03 +0000

    Disable prerendering
    Addition of webAuthn caused Problems.

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -19,18 +19,18 @@ import FeedingLayout from './layouts/FeedingLayout/FeedingLayout'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
-      <Route path="/login" page={LoginPage} name="login" prerender />
+      <Route path="/login" page={LoginPage} name="login" />
       {/* <Route path="/signup" page={SignupPage} name="signup" /> */}
-      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" prerender />
-      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" prerender />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
 
       <Private unauthenticated="login" wrap={MainLayout} whileLoadingAuth={() => <WhileLoadingAuth />}>
         <Route path="/diaperchange" page={DiaperChangePage} name="diaperChange" />
 
         <Set wrap={WeightsLayout}>
-          <Route path="/weights/new" page={WeightNewWeightPage} name="newWeight" prerender />
+          <Route path="/weights/new" page={WeightNewWeightPage} name="newWeight" />
           <Route path="/weights/{id:Int}/edit" page={WeightEditWeightPage} name="editWeight" />
-          <Route path="/weights" page={WeightWeightsPage} name="weights" prerender />
+          <Route path="/weights" page={WeightWeightsPage} name="weights" />
         </Set>
 
         <Set wrap={FeedingLayout}>
@@ -40,7 +40,7 @@ const Routes = () => {
         </Set>
 
         <Route path="/user" page={UserPage} name="user" />
-        <Route path="/" page={HomePage} name="home" prerender />
+        <Route path="/" page={HomePage} name="home" />
       </Private>
 
       <Route notfound page={NotFoundPage} />
